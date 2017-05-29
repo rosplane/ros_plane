ee280e1a5a7d9638f2131ac0d477be53c4f9310f
Jesse Wynn <wynnsquad@gmail.com>
2017-05-29 09:45:50 -0600

    failsafe callable from ground station

--- src/failsafe.py
+++ src/failsafe.py
@@ -15,21 +15,34 @@ def failsafe_callback(msg):
 				if time > 180: # If in failsafe for 3 mins
 					Terminate.data = True
 	else:
-		RTH.data = False
-		Terminate.data = False
+		if not RTHNOW:
+			RTH.data = False
+		if not TERMINATENOW:
+			Terminate.data = False
 		new_failsafe = True
+
+def RTHNOW_callback(msg):
+	RTHNOW = msg.data
+
+def TERMINATENOW_callback(msg):
+	TERMINATENOW = msg.data
 
 new_failsafe = True
 new_failsafe_time = 0
-
-status_sub = rospy.Subscriber('status', Status, failsafe_callback)
-RTH_pub = rospy.Publisher('RTH', Bool, queue_size=10)
-Terminate_pub = rospy.Publisher('terminate_flight', Bool, queue_size=10)
+RTHNOW = False
+TERMINATENOW = False
 
 RTH = Bool()
 RTH.data = False
 Terminate = Bool()
 Terminate.data = False
+
+status_sub = rospy.Subscriber('status', Status, failsafe_callback)
+RTHNOW_sub = rospy.Subscriber('RTH_now', Bool, RTHNOW_callback)
+TERMINATENOW_sub = rospy.Subscriber('TERMINATE_now', Bool, TERMINATENOW_callback)
+RTH_pub = rospy.Publisher('RTH', Bool, queue_size=10)
+Terminate_pub = rospy.Publisher('terminate_flight', Bool, queue_size=10)
+
 
 rospy.init_node('Status Watcher')
 r = rospy.Rate(10) # 10hz
